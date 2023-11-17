0104ba821efb42f59679ea03e6912c43404c5dde
Chork Hieng <hieng001@umn.edu>
2023-11-17 01:12:52 -0600

    add more files and update info

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set("view engine", "pug");
 app.use(express.static('resources'));
 
 // run middle to show http request and response
-// on terminal
+// on terminal to response's status code and message
 app.use((req, res, next)=> {
     console.log(req.method, `localhost:${port}${req.path}`);
     // run the actual request handler
@@ -17,16 +17,34 @@ app.use((req, res, next)=> {
     console.log(res.statusCode, res.statusMessage);
 })
 
+// get main page
 app.get("/", (req, res)=>{
     res.render("main.pug");
 })
 
-app.get("/images/index.jpeg", (req, res)=>{
-    res.send("index.jpeg");
+// get project page
+app.get("/projects", (req, res)=>{
+    res.render("projects.pug");
 })
 
+// get resume page
+app.get("/resume", (req, res)=>{
+    res.render("resume.pug");
+})
+
+// get image for main page
+app.get("/images/index.jpeg", (req, res)=>{
+    res.send("me.jpeg");
+})
+
+// get css style for normal mode
 app.get("/css/main.css", (req, res)=>{
     res.send("main.css");
+})
+
+// get css style for dark mode
+app.get("/css/main.dark.css", (req, res)=>{
+    res.send("main.dark.css");
 })
 
 // listen to port 8000
